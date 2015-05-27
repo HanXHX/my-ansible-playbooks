bc2be853a8cdc1f7f7f89d91259ba98ac46b021b
HanXHX <admin@debianiste.org>
2015-05-27 15:14:04 +0200

    fix check_syntax.sh, now works everywhere

--- check_syntax.sh
+++ check_syntax.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ANSIBLE_FORCE_COLOR=true ANSIBLE_HOST_KEY_CHECKING=false PYTHONUNBUFFERED=1 ansible-playbook --private-key=/home/hanx/.vagrant.d/insecure_private_key --user=vagrant --limit='default' --inventory-file=/home/hanx/dev/my-ansible-playbooks/.vagrant/provisioners/ansible/inventory --sudo -vvv --syntax-check playbook.yml
+ANSIBLE_FORCE_COLOR=true ANSIBLE_HOST_KEY_CHECKING=false PYTHONUNBUFFERED=1 ansible-playbook --private-key=$HOME/.vagrant.d/insecure_private_key --user=vagrant --limit='default' --inventory-file=$PWD/.vagrant/provisioners/ansible/inventory --sudo -vvv --syntax-check playbook.yml
